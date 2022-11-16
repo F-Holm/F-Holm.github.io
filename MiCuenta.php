f71c413ebca9d0bdf191a6f225ead147981758ea
F-Holm <101185761+F-Holm@users.noreply.github.com>
2022-11-16 15:54:14 -0300

    Update MiCuenta.php

--- MiCuenta.php
+++ MiCuenta.php
@@ -11,6 +11,24 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
+    <script>
+        /*function login(){
+            $.ajax({
+                method: "GET",
+                url:"DBLogin.php",
+                data: { Usuario: $("#UsuarioLogin").val(), Contrasena: $("#Contrasena").val()}
+            })
+            .done(function(msg){
+            alert(msg);
+            if(msg=="SESIÓN INICIADA"){
+                location.href ="Incio.html";
+            }
+            if(msg=="ERROR"){
+                location.href = "Login.html";
+            }
+            });
+        }*/
+    </script>
 </head>
 
 <body bgcolor="black">
